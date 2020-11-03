ea7ef874a7be068d8085620575dfbb9a734acd2b
Lenovo <hiteshk567@gmail.com>
2020-11-03 23:04:23 +0530

    3rd commit

--- docs/resetPage.js
+++ docs/resetPage.js
@@ -4,17 +4,20 @@ let resetPass = async () => {
   let email = document.querySelector("#email").value;
 
   try {
-    let response = await fetch("http://localhost:3000/user/newPassword", {
-      method: "POST",
-      body: JSON.stringify({
-        email: email,
-        resetString: randomString,
-        password: newPassword,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    let response = await fetch(
+      "https://resetpassworddbase.herokuapp.com/user/newPassword",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          email: email,
+          resetString: randomString,
+          password: newPassword,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
     let responseData = await response.json();
     alert(responseData.message);
   } catch (error) {
