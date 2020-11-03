2de36e1cb049820bea2d23b6bd843cddf1235585
Lenovo <hiteshk567@gmail.com>
2020-11-03 22:05:46 +0530

    initial commit

---
+++ docs/resetPage.js
@@ -0,0 +1,23 @@
+let resetPass = async () => {
+  let randomString = document.querySelector("#resetString").value;
+  let newPassword = document.querySelector("#newPassword").value;
+  let email = document.querySelector("#email").value;
+
+  try {
+    let response = await fetch("http://localhost:3000/user/newPassword", {
+      method: "POST",
+      body: JSON.stringify({
+        email: email,
+        resetString: randomString,
+        password: newPassword,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    let responseData = await response.json();
+    alert(responseData.message);
+  } catch (error) {
+    console.log(error);
+  }
+};
